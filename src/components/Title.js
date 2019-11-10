0e148720f72db40e504053ae7713b5ea05fb064c
Minh Tuan VO <vominhtuan164@gmail.com>
2019-11-10 20:54:29 +0100

    My first project in ReactJS

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Title(props) {
+
+
+    return (
+        <div className="page-header">
+            <h1>Project 01 - ToDo List <small>ReactJS</small></h1>
+        </div>
+    );
+
+}
+
+export default Title;
